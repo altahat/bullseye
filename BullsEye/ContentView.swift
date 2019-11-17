c9b3b3e99ee66e789dbc000cfd22ecd5aab05d35
zaid <alahat@uwp.edu>
2019-11-17 10:40:06 -0600

    Let's add some spacers and padding.

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,53 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var alertIsVisible = false
-        
-    var body: some View {
-        VStack {
-            //Target row
-            HStack {
-                Text("Put the bullseye as close as you can to:")
-                    .font(.largeTitle)
-                Text("100")
-            }
-            
-            //Slider row
-            HStack {
-              Text("1")
-              Slider(value: /*@START_MENU_TOKEN@*/.constant(10)/*@END_MENU_TOKEN@*/)
-              Text("100")
-            }
-            
-            //Button row
-            Button(action: {
-                print ("I was pressed")
-                self.alertIsVisible = true
-            }) {
-                Text("Hit me!")
-            }
-            .alert(isPresented: $alertIsVisible) { () -> Alert in
-              return Alert(title: Text("Hello there!"),
-                           message: Text("This is my first pop-up."),
-                           dismissButton: .default(Text("Awesome!")))
-            }
-            
-            //Score row
-            HStack {
-              Button(action: {}) {
-                Text("Start Over")
-              }
-              Text("Score:")
-              Text("999999")
-              Text("Round:")
-              Text("999")
-              Button(action: {}) {
-                Text("Info")
-              }
-            }
+
+  @State var alertIsVisible: Bool = false
+ 
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      // Target row
+      HStack {
+        Text("Put the bullseye as close as you can to:")
+        Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+      }
+      Spacer()
+      
+      // Slider row
+      HStack {
+        Text("1")
+        Slider(value: /*@START_MENU_TOKEN@*/.constant(10)/*@END_MENU_TOKEN@*/)
+        Text("100")
+      }
+      Spacer()
+      
+      // Button row
+      Button(action: {
+        print("Button pressed!")
+        self.alertIsVisible = true
+      }) {
+        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+      }
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+      }
+      Spacer()
+      
+      // Score row
+      HStack {
+        Button(action: {}) {
+          Text("Start Over")
         }
+        Spacer()
+        Text("Score:")
+        Text("999999")
+        Spacer()
+        Text("Round:")
+        Text("999")
+        Spacer()
+        Button(action: {}) {
+          Text("Info")
+        }
+      }
+      .padding(.bottom, 20)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
