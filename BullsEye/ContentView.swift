b62fe087aa67585d8c4a7c9127f10e170a74e411
zaid <alahat@uwp.edu>
2019-11-17 22:45:56 -0600

    Customized alert message + some refactoring.

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
       }
       .alert(isPresented: $alertIsVisible) { () -> Alert in
         //let roundedValue = Int(sliderValue.rounded())
-        return Alert(title: Text("Hello there!"), message: Text(
+        return Alert(title: Text(alertTitle()), message: Text(
           "The slider's value is \(sliderValueRounded()).\n" +
           "You scored \(pointsForCurrentRound()) points this round."
         ), dismissButton: .default(Text("Awesome!")) {
@@ -80,8 +80,27 @@ struct ContentView: View {
     Int(sliderValue.rounded())
   }
   
+  func amountOff() -> Int {
+    abs(target - sliderValueRounded())
+  }
+  
   func pointsForCurrentRound() -> Int {
-    100 - abs(target - sliderValueRounded())
+    100 - amountOff()
+  }
+  
+  func alertTitle() -> String {
+    let difference = amountOff()
+    let title: String
+    if difference == 0 {
+      title = "Perfect!"
+    } else if difference < 5 {
+      title = "You almost had it!"
+    } else if difference <= 10 {
+      title = "Not bad."
+    } else {
+      title = "Are you even trying?"
+    }
+    return title
   }
   
 }
